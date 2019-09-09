3833723bfa7ee4207fc572f7c8bd29ed521c00d1
dabudidabuday <94mir.ms@gmail.com>
2019-09-09 21:29:47 +0300

    wip

--- src/controller/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app, router) {
-  const home = require('./HomePageController.js')(router);
-  const developer = require('./DeveloperPageController.js')(router);
-
-  app.use(home);
-  app.use(developer);
-}
