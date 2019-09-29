10de6f4d9682331b64254203cb14da104a932ac5
roman bogdanov <cflatminor92@gmail.com>
2019-09-29 13:59:58 +0300

    merge oshii

--- src/model/HomePageModel.js
+++ src/model/HomePageModel.js
@@ -1,4 +1,3 @@
-
 const Resolver = require('./resolver.js');
 
 class HomePageModel {
@@ -6,20 +5,6 @@ class HomePageModel {
     this.title = 'HomePage';
     this.breeds = Resolver.getBreeds();
   }
-
-  setHomePageData (data) {
-    console.log(data);
-  }
-
-
-
-  // async countBreeds () {
-  //   const breeds = await this.getBreeds()
-  //
-  //   if (breeds.data.message) {
-  //     setHomePageData(breeds.data.message);
-  //   }
-  // }
 }
 
 module.exports = new HomePageModel ();
--- src/model/resolver.js
+++ src/model/resolver.js
@@ -1,14 +1,6 @@
 const axios = require('axios');
 
 class Resolver {
-  constructor () {
-
-  }
-
-  loga () {
-    console.log('a');
-  }
-
   async getBreeds () {
     try {
       return await axios.get('https://dog.ceo/api/breeds/list/all')
