c030e05ce98c7a5d6d0d289ab1a287dfb6fe4370
dabudidabuday <94mir.ms@gmail.com>
2019-10-22 18:55:38 +0300

    creating greed 0f goods

--- src/entity/Product.js
+++ src/entity/Product.js
@@ -5,6 +5,7 @@
   */
 class Product {
   constructor(product) {
+    this.image = 'https://static.dnipro-m.ua/cache/products/1237/catalog_origin_162421.jpg';
     this.title = product.title;
     this.prices = product.prices;
     this.brand = product.brand;
